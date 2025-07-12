eadb9024623cfe81b3666205c439201ea930f3b0
torwin-job <andreibuharikov@yandex.ru>
2025-07-12 16:17:15 +0700

    Update aggregations.py

--- aggregations.py
+++ aggregations.py
@@ -1,23 +1,23 @@
 from abc import ABC, abstractmethod
-from typing import List, Any
+from typing import Any
 
 
 class Aggregation(ABC):
     @abstractmethod
-    def aggregate(self, values: List[float]) -> Any:
+    def aggregate(self, values: list[float]) -> Any:
         pass
 
 
 class AvgAggregation(Aggregation):
-    def aggregate(self, values: List[float]) -> float | None:
+    def aggregate(self, values: list[float]) -> float | None:
         return sum(values) / len(values) if values else None
 
 
 class MinAggregation(Aggregation):
-    def aggregate(self, values: List[float]) -> float:
+    def aggregate(self, values: list[float]) -> float:
         return min(values)
 
 
 class MaxAggregation(Aggregation):
-    def aggregate(self, values: List[float]) -> float:
+    def aggregate(self, values: list[float]) -> float:
         return max(values)
