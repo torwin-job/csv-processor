0893c61baa660e1c4072381bc4c5c1f35cb5d6fd
torwin-job <andreibuharikov@yandex.ru>
2025-07-12 16:16:32 +0700

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,13 +1,12 @@
 import csv
 from models import CSVRow
-from typing import List
 
 
 class CSVReader:
     def __init__(self, file_path: str) -> None:
         self.file_path: str = file_path
 
-    def read(self) -> List[CSVRow]:
+    def read(self) -> list[CSVRow]:
         with open(self.file_path, newline="", encoding="utf-8") as f:
             reader = csv.DictReader(f)
             return [CSVRow(data=row) for row in reader]
